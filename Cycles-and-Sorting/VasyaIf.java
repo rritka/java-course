8a209094506d7d2fc8858f524fec5ec3fb3abca7
Rita Romanenka <margaritaromanenkomr@gmail.com>
2020-02-05 01:13:14 +0300

    Must be better for Ilya

--- Cycles-and-Sorting/VasyaIf.java
+++ Cycles-and-Sorting/VasyaIf.java
@@ -11,16 +11,20 @@ public class VasyaIf {
 
         if (Objects.equals(name, "Вася")) {
             System.out.println("Привет, Вася! \n" + "Я тебя так долго ждал!");
+            return;
         }
         if (Objects.equals(name, "Анастасия")) {
             System.out.println("Привет, Анастасия! \n" + "Я скучал!");
+            return;
         }
         if (Objects.equals(name, "Илья")) {
             System.out.println("Привет, Ильюша! \n" + "Обожаю твою ямочку на щеке!");
+            return;
         }
 
         if (Objects.equals(name, "Рита")) {
             System.out.println("Привет, Ритка - лиска! :)");
+            return;
         }
 
         if (!Objects.equals(name, "Анастасия") && !Objects.equals(name, "Вася") &&
