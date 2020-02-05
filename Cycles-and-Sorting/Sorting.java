1f1c344e2782b30219616e746176565c3ab95877
Rita Romanenka <margaritaromanenkomr@gmail.com>
2020-02-05 18:18:25 +0300

    + bubble sort

--- Cycles-and-Sorting/Sorting.java
+++ Cycles-and-Sorting/Sorting.java
@@ -3,36 +3,34 @@ import java.util.Scanner;
 public class Sorting {
 
     public static void main(String[] args) {
-
+/*
         Scanner in = new Scanner(System.in);
         System.out.print("Put length of Array: \n");
-        int len = in.nextInt();
+        int len = in.nextInt(); */
+int len = 7;
 
-        //создается рандомный массив и выводится в консоль
-        System.out.print("Random Array: \n");
+        System.out.print("Random Array: \n");    //создается рандомный массив и выводится в консоль
         int ar[] = new int[len];
         for (int i = 0; i < ar.length; i++) {
             ar[i] = (int) (Math.random() * 10);
             System.out.print(ar[i] + " ");
         }
 
-        //вызываю метод сортировки
-        ar = sortChoosingMin(ar);
+        //ar = sortChoosingMin(ar);                //вызываю метод сортировки выбором
+        ar = sortBubble(ar);                //вызываю метод сортировки пузырьком
 
-        //вывожу отсортированный массив
-        System.out.println("\nSort Array:" );
+        System.out.println("\nSort Array:" );     //вывожу отсортированный массив
         for (int i = 0; i < ar.length; i++) {
             System.out.print(ar[i] + " ");
         }
     }
-        public static int[] sortChoosingMin(int[] ar) {
+        public static int[] sortChoosingMin(int[] ar) { //сортировка вставками
             int indexMin = 0;
-            int min = ar[0];
-
+            int min = 0;
             for (int i = 0; i < ar.length; i++) {
+                indexMin = i;
                 min = ar[i];
                 for (int j = i; j < ar.length; j++) {
-
                     if (ar[j] < min) {
                         min = ar[j];
                         indexMin = j;
@@ -42,6 +40,22 @@ public class Sorting {
                 ar[i] = min;
             }
             return ar;
-
         }
+
+        public static int[] sortBubble(int[] ar) { // сортировка пузырьком
+        int j = 1;
+        int k = 0;
+            do {
+                j++;
+                for (int i = 0; i < (ar.length - 1); i++) {
+                    if (ar[i] > ar[i + 1]) {
+                        k = ar[i];
+                        ar[i] = ar[i + 1];
+                        ar[i + 1] = k;
+                    }
+                }
+            } while (1 <= (ar.length - j));
+            return ar;
+        }
+
     }
