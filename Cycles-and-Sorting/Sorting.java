6458f618bacc807f45e0e921b5f468f398d983c8
Rita Romanenka <margaritaromanenkomr@gmail.com>
2020-02-07 17:00:44 +0300

    + merge sort and measure time

--- Cycles-and-Sorting/Sorting.java
+++ Cycles-and-Sorting/Sorting.java
@@ -7,25 +7,56 @@ public class Sorting {
         Scanner in = new Scanner(System.in);
         System.out.print("Put length of Array: \n");
         int len = in.nextInt(); */
-int len = 10;
+int len = 100_000;
 
-        System.out.print("Random Array: \n");    //создается рандомный массив и выводится в консоль
+       // System.out.print("Random Array: \n");    //создается рандомный массив и выводится в консоль
         int ar[] = new int[len];
-        for (int i = 0; i < ar.length; i++) {
-            ar[i] = (int) (Math.random() * 10);
-            System.out.print(ar[i] + " ");
-        }
+        int ar1[] = new int[len];
+        int ar2[] = new int[len];
+        int ar3[] = new int[len];
+        int ar4[] = new int[len];
 
+        /*
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = ar1[i] = ar2[i] = ar3[i] = ar4[i] = (int) (Math.random() * 10);
+          //  System.out.print(ar[i] + " ");
+        } */
+
+        System.out.println("Time of sort by choosing min");
+        measureTime(() -> sortChoosingMin(ar));
         //ar = sortChoosingMin(ar);            //вызываю метод сортировки выбором
-        //ar = sortBubble(ar);                 //вызываю метод сортировки пузырьком
-        ar = sortCocktail(ar);                 //вызываю метод сортировки шейкерной
-        //ar = sortInsert(ar);                 //вызываю метод сортировки вставками
 
-        System.out.println("\nSort Array:" );     //вывожу отсортированный массив
+        System.out.println("Time of bubble sort");
+        measureTime(() -> sortBubble(ar1));
+        //ar1 = sortBubble(ar1);                 //вызываю метод сортировки пузырьком
+
+        System.out.println("Time of Shake sort");
+        measureTime(() -> sortCocktail(ar2));
+        //ar2 = sortCocktail(ar2);                 //вызываю метод сортировки шейкерной
+
+        System.out.println("Time of Insert sort");
+        measureTime(() -> sortInsert(ar3));
+        //ar3 = sortInsert(ar3);                 //вызываю метод сортировки вставками
+
+        System.out.println("Time of Merge sort");
+        measureTime(() -> sortMerge(ar4));
+        //ar4 = sortMerge(ar4);                 //вызываю метод сортировки слиянием
+
+
+       /*
+        System.out.println("\nSort Array:" );     //вывод отсортированного массивa
         for (int i = 0; i < ar.length; i++) {
             System.out.print(ar[i] + " ");
-        }
+        } */
     }
+
+    public static void measureTime(Runnable sort){    //метод расчета времени выполнения сортирки
+        long start = System.currentTimeMillis();
+        sort.run();
+        long delta = System.currentTimeMillis() - start;
+        System.out.println("Running time: " + delta + " ms\n");
+    }
+
         public static int[] sortChoosingMin(int[] ar) { //сортировка вставками
             int indexMin = 0;
             int min = 0;
@@ -94,5 +125,48 @@ int len = 10;
         }
         return ar;
         }
+
+    public static int[] sortMerge (int[] ar) { //сортировка слиянием
+        int[] tmp;
+        int[] currentSrc = ar;
+        int[] currentDest = new int[ar.length];
+
+        int size = 1;
+        while (size < ar.length) {
+            for (int i = 0; i < ar.length; i += 2 * size) {
+                merge(currentSrc, i, currentSrc, (i + size), currentDest, i, size);
+            }
+
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+
+            size = size * 2;
+
+        }
+        return currentSrc;
+    }
+
+    private static void merge(int[] a, int aStart, int[] b, int bStart, int[] dest,  // метод для слияния массивов
+                              int destStart, int size) {
+        int indexA = aStart;
+        int indexB = bStart;
+
+        int aEnd = Math.min(aStart + size, a.length);
+        int bEnd = Math.min(bStart + size, b.length);
+
+        int iterationCount = aEnd - aStart + bEnd - bStart;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (indexA < aEnd && (indexB >= bEnd || a[indexA] < b[indexB])) {
+                dest[i] = a[indexA];
+                indexA++;
+            } else {
+                dest[i] = b[indexB];
+                indexB++;
+            }
+        }
+    }
+
 
 }
