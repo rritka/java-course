c5f50d931c9d13f1d20e6a0da487564f693f355d
Rita Romanenka <margaritaromanenkomr@gmail.com>
2020-08-08 21:10:15 +0300

    must be right algorithm

--- dartz/Main.java
+++ dartz/Main.java
@@ -1,118 +1,121 @@
 import java.util.Arrays;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            //input data without black    (answer must be 8)
-//            int data[] = {1, -3, 2, -2, 3, 4};
-//            int n = 6;
-//            int k = -1;
+        //input data without black    (answer must be 8)
+//        int data[] = {1, -3, 2, -2, 3, 4};
+//        int n = 6;
+//        int k = -1;
 
-            //input data with black   (answer must be 12)
+        //input data with black   (answer must be 12)
             int data[] = {2, 3, 4, 5, -30, 6, -1, 2};
             int n = 8;
             int k = 2;
 
-            int result = 0;
+        int result = 0;
 
-            if (k == -1) {
-                result = gameNoBlack(data);
-                System.out.println(result);
-            } else if (k >= 0) {
-                result = gameWithBlack(data, k);
-                System.out.println(result);
-            } else {
-                System.out.println("error k < -1");
-            }
-
+        if (k == -1) {
+            result = gameNoBlack(data, n);
+            System.out.println(result);
+        } else if (k >= 0) {
+                result = gameWithBlack(data, n, k);
+            System.out.println(result);
+        } else {
+            System.out.println("error k < -1");
         }
 
+    }
 
-    public static int gameNoBlack(int[] data){
-        //локальная сумма очков для одной итерации между красным и синим дротиком
-        int sum = 0;
+
+    public static int gameNoBlack(int[] data, int n) {
 
         //массив значений максимальных сумм от каждой секции
-        int[] all_sum = new int[data.length];
+        int max = 0;
 
-        //нахождение максимальной суммы от каждой секции и запись в массив all_sum
-        for (int i = 0; i < data.length; i++) {  // i - красная метка
-            sum = data[i];
+        //промежуточные суммы от синей метки до каждой секции
+        int[] sum = new int[n];
+
+        //максимальные значения от каждой секции
+        int[] sum_of_max = new int[n];
+
+        //нахождение максимальной суммы от каждой секции и запись в массив
+        for (int i = 0; i < n-1; i++) {  // i - красная метка
             int blue = i + 1;                    // blue - синяя метка
-            do {
-                if(blue == data.length) {   // проход по кругу массива, т.е если дошли до конца -
+            int j = 0;
+            sum[j] = data[i];
+
+            while (blue != i) {
+                sum[j+1] = sum[j] + data[blue];
+                if (blue == n-1) {        // проход по кругу массива, т.е если дошли до конца -
                     blue = 0;               //  - начать сначала, пока не дойдем до того же индекса(усл в while)
-                }
-                int buff = sum + data[blue];
-                if (sum <= buff) {          //если сумма + следующее больше, то присваеваем новую сумму
-                    sum = buff;
-                } else {
-                    all_sum[i] = sum;       //если нет, записываем значение и переставляем красную метку
-                    break;
-                }
-                blue++;
-            } while(blue != i + 1);
+                } else blue++;
+                j++;
+            }
+            //выбор максимального значения из сумм от каждой секции
+            sum_of_max[i] = sum[0];
+            for (int k = 0; k < n; k++) {
+                sum_of_max[i] = Math.max(sum[k], sum_of_max[i]);
+            }
         }
 
         //выбор максимального значения из максимальных сумм от каждой секции
-        int max = all_sum[0];
-        for (int i = 0; i < all_sum.length; i++) {
-            max = Math.max(all_sum[i],max);
+        max = sum_of_max[0];
+        for (int k = 0; k < n; k++) {
+            max = Math.max(sum_of_max[k], max);
+
         }
         return max;
     }
 
 
-
-    public static int gameWithBlack(int[] data, int k){
-        //локальная сумма очков для одной итерации между красным и синим дротиком
-        int sum = 0;
+    public static int gameWithBlack(int[] data, int n, int k) {
 
         //массив значений максимальных сумм от каждой секции
-        int[] all_sum = new int[data.length];
+        int max = 0;
 
-        //нахождение максимальной суммы от каждой секции и запись в массив all_sum
-        for (int i = 0; i < data.length; i++) {  // i - красная метка
+        //промежуточные суммы от синей метки до каждой секции
+        int[] sum = new int[n];
 
+        //максимальные значения от каждой секции
+        int[] sum_of_max = new int[n];
+
+        //нахождение максимальной суммы от каждой секции и запись в массив
+        for (int i = 0; i < n-1; i++) {  // i - красная метка
+            if (i == k) continue;
             int blue = i + 1;                    // blue - синяя метка
+            int j = 0;
+            sum[j] = data[i];
 
-            if (i == k){
-                sum = 0;
-                all_sum[i] = sum;
-            } else {
-                sum = data[i];
-
-                do {
-                    if (blue == data.length) {   // проход по кругу массива, т.е если дошли до конца -
-                        blue = 0;               //  - начать сначала, пока не дойдем до того же индекса(усл в while)
-                    }
-                    if (blue != k) {
-                        int buff = sum + data[blue];
-                        if (sum <= buff) {          //если сумма + следующее больше, то присваеваем новую сумму
-                            sum = buff;
-                        } else {
-                            all_sum[i] = sum;       //если нет, записываем значение и переставляем красную метку
-                            break;
-                        }
-                    } else {
-                        sum = 0;
-                        all_sum[i] = sum;       //если нет, записываем значение и переставляем красную метку
-                        break;
-                    }
-
-                    blue++;
-                } while (blue != i + 1);
+            while (blue != i) {
+                if (blue == k){
+                    sum[j+1] = 0;
+                    break;
+                }
+                sum[j+1] = sum[j] + data[blue];
+                if (blue == n-1) {        // проход по кругу массива, т.е если дошли до конца -
+                    blue = 0;               //  - начать сначала, пока не дойдем до того же индекса(усл в while)
+                } else blue++;
+                j++;
+            }
+            //выбор максимального значения из сумм от каждой секции
+            sum_of_max[i] = sum[0];
+            for (int l = 0; l < n; l++) {
+                sum_of_max[i] = Math.max(sum[l], sum_of_max[i]);
             }
         }
 
         //выбор максимального значения из максимальных сумм от каждой секции
-        int max = all_sum[0];
-        for (int i = 0; i < all_sum.length; i++) {
-            max = Math.max(all_sum[i],max);
-        }
-            return max;
-    }
+        max = sum_of_max[0];
+        for (int l = 0; l < n; l++) {
+            max = Math.max(sum_of_max[l], max);
 
+        }
+        return max;
+    }
 }
+
+
+
 
 
